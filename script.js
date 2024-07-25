3f3a24826bcbf7c0620c3692566e9d1ef064353c
Syed <muhtasham00@gmail.com>
2024-07-25 15:16:10 +0500

    complete

--- script.js
+++ script.js
@@ -8,14 +8,23 @@ getData = () => {
       let data = response.json().then((data) => {
         console.log({ data });
         const name = data.results[0].name.first;
-        const image = data.results[0].picture.medium;
-        console.log({ name, image });
+        const email = data.results[0].email;
+        const cell = data.results[0].cell;
+        const dob = data.results[0].dob.age;
+        const gender = data.results[0].gender;
+        const location = data.results[0].location.city;
+        const img = data.results[0].picture.medium;
 
-        const profileDiv = document.getElementById('data');
-      profileDiv.innerHTML = `
-        <h1>${name}</h1>
-        <img src="${image}" alt="Profile Picture of ${name}">
-      `;
+        
+        // console.log({ name, img });
+
+    document.getElementById('name').innerHTML=`Name: ${name}`;
+    document.getElementById('email').innerHTML=`Email:${email}`;
+    document.getElementById('cell').innerHTML=`Phone: ${cell}`;
+    document.getElementById('dob').innerHTML=`Age: ${dob} Years`;
+    document.getElementById('gender').innerHTML=`gender: ${gender}`;
+    document.getElementById('location').innerHTML=`Location: ${location}`;
+    document.getElementById('img').innerHTML=`<img src="${img}"/>`;
 
 
       });

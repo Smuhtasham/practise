16ffb2e18b0b162f3dffbab64fa8b4d4641c674e
Syed <muhtasham00@gmail.com>
2024-07-27 17:04:01 +0500

    structure

--- script.js
+++ script.js
@@ -4,22 +4,20 @@ getData = () => {
   fetch(url)
     .then((response) => {
       let data = response.json().then((data) => {
-        console.log({data});
+        console.log({ data });
         let userData = data.results.map((data) => {
-          return (
-            `<div style="align-items:center; justify-content:center;">
+          return `<div style="align-items:center; justify-content:center;">
              <span> Name: ${data.name.first}</span>
                <span>Email:${data.email}</span>
                 <span>Phone: ${data.cell}</span>
                  <span>Age: ${data.dob.age} Years </span>
                   <span>gender: ${data.gender}</span>
                    <span>Location: ${data.location.city}</span>
-              <span><img src="${data.picture.medium}" /></span></div>  `
-          );
-         
+              <span><img src="${data.picture.medium}" /></span></div>  `;
         });
-         document.getElementById("data").innerHTML = userData;
-      }); console.log(data)
+        document.getElementById("data").innerHTML = userData;
+      });
+      console.log(data);
     })
     .catch((reject) => {
       return console.log(reject);
